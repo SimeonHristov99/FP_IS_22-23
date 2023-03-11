c998b00dba9004308bd5b15c5cea5f61bacbed8e
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-03-11 17:41:36 +0200

    Add tasks week 4.

---
+++ Week_04 - Lists. Higher Order Functions/task9.hs
@@ -0,0 +1,9 @@
+main :: IO()
+main = do
+    print $ getPrimesLC 1 100 == [7,17,37,47,67,71,73,79,97]
+    print $ getPrimesLC 100 1 == [7,17,37,47,67,71,73,79,97]
+
+    print $ getPrimesHOF 1 100 == [7,17,37,47,67,71,73,79,97]
+    print $ getPrimesHOF 100 1 == [7,17,37,47,67,71,73,79,97]
+
+
